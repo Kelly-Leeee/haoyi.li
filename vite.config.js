c37e7c1ee450089b5d3c1a796b1e719f4a5967d9
Kelly-Leeee <writerqianxia@gmail.com>
2026-05-25 22:33:25 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  base: '/haoyi.li/',
-})
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/haoyi.li/' : '/',
+}))
